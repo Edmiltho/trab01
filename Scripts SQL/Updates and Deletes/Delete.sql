63ab6cc29c5160bdd543f411d81ec327355c8b2f
ThiagoFelicioAlves <30706511+ThiagoFelicioAlves@users.noreply.github.com>
2018-07-02 20:47:56 -0300

    Create Delete.sql

---
+++ Scripts SQL/Updates and Deletes/Delete.sql
@@ -0,0 +1,17 @@
+DELETE FROM paga
+WHERE valor = 50;
+
+DELETE FROM veiculo
+WHERE placa_veiculo = 'AVC-2134';
+
+DELETE FROM paga
+WHERE valor = 126;
+
+DELETE FROM sensor
+WHERE id_sensor = 4;
+
+DELETE FROM cartao
+WHERE nome_impresso = 'João Silva';
+
+DELETE FROM cartao
+WHERE nome_impresso = 'Felicia';
